62fe7efae5e7ea8e7d766c547b385cb441542ad9
gaboroa14 <gaboroab14@gmail.com>
2019-11-11 20:57:35 -0400

    las solicitudes no se despliegan

--- src/java/eis/SolicitudesDaoImpl.java
+++ src/java/eis/SolicitudesDaoImpl.java
@@ -25,12 +25,35 @@ public class SolicitudesDaoImpl implements SolicitudesDao{
     
     @Override
     public List<Solicitudes> listadoSolicitudes(){
-        return em.createNamedQuery("Solicitudes.findAll").getResultList();
+        Query query = em.createNamedQuery("Solicitudes.findByFechaInicioInterval");
+        Calendar c = Calendar.getInstance();
+        int mes = c.get(Calendar.MONTH);
+        int anno = c.get(Calendar.YEAR);
+        Calendar primero_mes = new GregorianCalendar(anno, mes, 1);
+        Calendar ultimo_mes;
+        try{
+            ultimo_mes = new GregorianCalendar(anno, mes, 31);
+        } catch (Exception e){
+            try{
+                ultimo_mes = new GregorianCalendar(anno, mes, 30);
+            } catch (Exception x){
+                try{
+                    ultimo_mes = new GregorianCalendar(anno, mes, 29);
+                } catch (Exception f){
+                    ultimo_mes = new GregorianCalendar(anno, mes, 28);
+                }
+            }
+        }
+        Date primero = primero_mes.getTime();
+        Date ultimo = ultimo_mes.getTime();
+        query.setParameter("primeroMes", primero);
+        query.setParameter("finMes", ultimo);
+        return (List<Solicitudes>) query.getResultList();
     }
     
     @Override
     public List<Solicitudes> listadoSolicitudesFiltradas(int mes, int anno){
-        Query query = em.createNamedQuery("Solicitudes.findByFechaInicio");
+        Query query = em.createNamedQuery("Solicitudes.findByFechaInicioInterval");
         mes--;
         Calendar primero_mes = new GregorianCalendar(anno, mes, 1);
         Calendar ultimo_mes;
